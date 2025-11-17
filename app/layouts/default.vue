4c7c8daed1badb37598d5f2d04e0057e4087b853
mosalehkamali <mosalehkamali@gmial.com>
2025-11-17 17:49:42 +0330

    first-commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1 class="text-3xl font-bold underline">Zoodex pickup layout!</h1>
+    <NuxtPage /> 
+  </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1 class="text-3xl font-bold underline">Zoodex pickup soon!</h1>
+  
+</template>
