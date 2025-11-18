e74641aac091905247533e63728869e29a0c77c4
mosalehkamali <mosalehkamali@gmial.com>
2025-11-19 00:14:55 +0330

    Add BaseBadge, BaseLoader, and BaseSkeletonBox components; update index.vue to include BaseSkeletonBox for loading state representation.

---
+++ app/components/ui/BaseSkeletonBox.vue
@@ -0,0 +1,38 @@
+<template>
+    <div :class="wrapperClasses" :style="wrapperStyle"></div>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    width: { type: String, default: null },
+    height: { type: String, default: null },
+    radius: { type: String, default: "8px" },
+    circle: { type: Boolean, default: false },
+    animated: { type: Boolean, default: true },
+    class: { type: String, default: "" },
+  })
+  
+  const wrapperClasses = computed(() => {
+    return [
+      "bg-gray-700 dark:bg-stone-300",
+      props.animated ? "animate-pulse" : "",
+      props.class,
+    ]
+  })
+  
+  const wrapperStyle = computed(() => {
+    const style = {}
+  
+    if (props.width) style.width = props.width
+    if (props.height) style.height = props.height
+  
+    if (props.circle) {
+      style.borderRadius = "50%"
+    } else {
+      style.borderRadius = props.radius
+    }
+  
+    return style
+  })
+  </script>
+  
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,8 @@
 <template>
   <h1 class="text-3xl font-bold underline">Zoodex pickup soon!</h1>
-  
+  <UiBaseSkeletonBox radius="1rem" height="100px" width="200px"/>
 </template>
+
+<script setup>
+
+</script>
