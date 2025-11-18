31d8f1cd6b95534c7aa58b364b2ad0cfb799a5fd
mosalehkamali <mosalehkamali@gmial.com>
2025-11-19 01:16:48 +0330

    Add BaseBadge component with customizable label and classes; implement template and script setup for styling and props management.

---
+++ app/components/ui/BaseBadge.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="wrapperClasses">
+    {{ label }}
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: { default: "" },
+  customClass: { default: "" },
+});
+
+const wrapperClasses = computed(() => {
+  return [
+    "inline-flex items-center bg-emerald-500 text-white rounded-full px-2 py-0.5 text-xs font-medium",
+    props.customClass,
+  ];
+});
+</script>
