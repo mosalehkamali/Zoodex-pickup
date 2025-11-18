b79cf69ea23c80b28cd76791ace2057cba469438
mosalehkamali <mosalehkamali@gmial.com>
2025-11-18 22:33:50 +0330

    Add restaurant store with state management and API integration; implement actions for fetching restaurants, selecting a restaurant, and setting categories, along with error handling and filtering functionality.

---
+++ app/stores/useRestaurants.js
@@ -0,0 +1,66 @@
+import { defineStore } from "pinia";
+
+export const useRestaurants = defineStore("restaurants", {
+  state: () => ({
+    restaurants: [],
+    selectedRestaurant: null,
+    selectedCategory: "",
+    loading: false,
+    error: null,
+  }),
+  actions: {
+    async fetchRestaurants() {
+      this.loading = true;
+      if (this.restaurants.length > 0) {
+        this.loading = false;
+        this.error = null;
+        return;
+      }
+      try {
+        const data = await $fetch("/api/restaurants");
+        this.restaurants = data;
+        this.error = null;
+      } catch {
+        this.error = "مشکلی پیش آمده، دوباره تلاش کنید";
+      }
+      this.loading = false;
+    },
+
+    setSelectedRestaurant(id) {
+      const restaurant = this.restaurants.find((item) => item.id === id);
+      if (restaurant) {
+        this.selectedRestaurant = restaurant;
+      } else {
+        this.selectedRestaurant = null
+        this.error = "مشکلی پیش آمده، دوباره تلاش کنید";
+      }
+    },
+
+    setCategory(category) {
+      this.selectedCategory = category;
+      this.error = null
+    },
+  },
+
+  getters: {
+    filteredRestaurants() {
+      if (this.selectedCategory) {
+        const filteredRestaurants = this.restaurants.filter(
+          (item) => item.category === this.selectedCategory
+        );
+        return filteredRestaurants;
+      } else {
+        return this.restaurants;
+      }
+    },
+
+    getRestaurantById(id) {
+      const restaurant = this.restaurants.find((item) => item.id === id);
+      if (restaurant) {
+        return restaurant;
+      } else {
+        return null;
+      }
+    },
+  },
+});
