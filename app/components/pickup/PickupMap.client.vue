8fb0e3cccee2386ffc68182d7fca4bb85d8b0eba
mosalehkamali <mosalehkamali@gmial.com>
2025-11-20 13:13:03 +0330

    Add PickupMap component for displaying restaurant locations on a map; update index.vue layout to include PickupMap and adjust styles for better responsiveness. Enhance AppLoader component with improved loading animation.

---
+++ app/components/pickup/PickupMap.client.vue
@@ -0,0 +1,110 @@
+<template>
+  <ClientOnly>
+    <div
+      ref="mapContainer"
+      class="w-full h-[100%] rounded-lg overflow-hidden"
+    ></div>
+  </ClientOnly>
+</template>
+
+<script setup>
+import { ref, onMounted, watch, computed, onBeforeUnmount } from 'vue'
+import { useRestaurants } from "~/stores/useRestaurants";
+
+const store = useRestaurants()
+const restaurants = ref(store.filteredRestaurants)
+
+const markers = computed(() =>
+  restaurants.value.map(res => ({
+    lat: res.location.lat,
+    lng: res.location.lng,
+    popup: res.name
+  }))
+)
+
+const mapContainer = ref(null)
+let map = null
+let markerLayer = null
+
+onMounted(async () => {
+  const leafletModule = await import("leaflet");
+  const L = leafletModule.default || leafletModule;
+  await import("leaflet/dist/leaflet.css");
+
+  map = L.map(mapContainer.value, {
+    center: [30.2839, 57.0714],
+    zoom: 13,
+    zoomControl: true,
+  });
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; OpenStreetMap contributors",
+    maxZoom: 19,
+  }).addTo(map);
+
+  markerLayer = L.layerGroup().addTo(map);
+
+  addMarkers(markers.value)
+
+  const ro = new ResizeObserver(() => map.invalidateSize());
+  ro.observe(mapContainer.value);
+
+  onBeforeUnmount(() => {
+    ro.disconnect()
+    map.remove()
+  })
+})
+
+function addMarkers(list) {
+  if (!map || !markerLayer) return;
+
+  markerLayer.clearLayers();
+
+  import('leaflet').then((leafletModule) => {
+    const L = leafletModule.default || leafletModule;
+
+    const customIcon = L.divIcon({
+      className: "restaurant-marker",
+      html: `<div class="${'dot'}"></div>`,
+      iconSize: [50, 50],
+      iconAnchor: [15, 30]
+    });
+
+    list.forEach(m => {
+      if (typeof m.lat === 'number' && typeof m.lng === 'number') {
+        const mk = L.marker([m.lat, m.lng ],{ icon: customIcon });
+        if (m.popup) mk.bindPopup(m.popup);
+        mk.addTo(markerLayer);
+      }
+    });
+  });
+}
+
+watch(
+  () =>   () => store.filteredRestaurants,
+  (newVal) => addMarkers(newVal),
+  { deep: true }
+)
+</script>
+
+
+<style scoped>
+.map-wrapper {
+  height: 100%;
+}
+:deep(.restaurant-marker){
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+:deep(.restaurant-marker .dot) {
+  width: 20px;
+  height: 20px;
+  background-color: #1447e6;
+  border-radius: 50%;
+  border: 3px solid white;
+  box-shadow: 0 0 6px rgba(0,0,0,0.3);
+}
+
+
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,19 @@
 <template>
- <div class="lg:h-screen mt-[5rem] grid grid-cols-1 lg:grid-cols-[60%_40%]">
-     <div class="overflow-visible lg:overflow-y-auto lg:h-screen">
-       <CategorySlider/> 
-      <RestaurantList/>
-     </div>
-  <div class="h-auto lg:sticky lg:top-0 lg:h-screen"></div>
- </div>
-  
+  <div class="lg:h-screen mt-[5rem] flex flex-col-reverse lg:grid grid-cols-1 lg:grid-cols-[60%_40%]">
+    <div class="overflow-visible lg:overflow-y-auto lg:h-screen">
+      <CategorySlider />
+      <RestaurantList />
+    </div>
+    <div class="h-[60vh] lg:h-auto lg:sticky lg:top-0 lg:h-screen">
+      <PickupMap />
+    </div>
+  </div>
 </template>
 
 <script setup>
-import RestaurantList from '~/components/pickup/RestaurantList.vue';
-import CategorySlider from '~/components/pickup/CategorySlider.vue';
+import RestaurantList from "~/components/pickup/RestaurantList.vue";
+import CategorySlider from "~/components/pickup/CategorySlider.vue";
+import PickupMap from "~/components/pickup/PickupMap.client.vue";
 import { useRestaurants } from "~/stores/useRestaurants";
 import { useUiStore } from "~/stores/useUiStore";
 
